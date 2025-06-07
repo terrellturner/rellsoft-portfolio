e31fdbb7bb9ad34a669361a39a266bd052f24ac6
terrellturner <terrell@rellsoft.dev>
2025-06-06 20:55:18 -0500

    More OG edits

--- rellsoft-blog-site/src/app/layout.tsx
+++ rellsoft-blog-site/src/app/layout.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
   description: "Junior full-stack web developer, game developer, 3D artist.",
   openGraph: {
     title: "Terrell Turner – Junior Full-Stack Dev",
-    description: "Practical ideas, clean UI, and always looking to learn.",
+    description: "Practical ideas, clean UI, always learning.",
     url: "https://rellsoft.dev",
-    siteName: "Terrell Turner",
+    siteName: "RellSoft",
     images: [
       {
         url: "./images/og-image.jpg",
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Terrell Turner – Junior Full-Stack Dev",
-    description: "Practical ideas, clean UI, and always looking to learn.",
+    description: "Practical ideas, clean UI, always learning.",
     images: ["./images/og-image.jpg"],
   },
   icons: {
