2cceb58be98b6b83d9b4a64ede7f9bae39f21182
terrellturner <terrell@rellsoft.dev>
2025-06-24 20:37:47 -0500

    .env files for sanity projectid/dataset

---
+++ rellsoft-blog/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'rellsoft-blog',
+
+  projectId: process.env.SANITY_PROJECT_ID || '',
+  dataset: process.env.SANITY_DATASET || '',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
